fa57e14ac40c2db7ac31bfb1229bc10f2d9f83be
Ahmad Hazim <hazimrizal6@gmail.com>
2023-08-27 15:24:48 +0800

    Create Homepage.php
    Created simple homepage

---
+++ Homepage.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>This is the Homepage</title>
+  <style>
+    body {
+      font-family: Arial, sans-serif;
+      line-height: 1.6;
+    }
+    nav {
+      background-color: #000;
+      color: #fff;
+      padding: 10px;
+    }
+    nav a {
+      text-decoration: none;
+      color: #fff;
+      padding: 5px;
+    }
+    nav a:hover {
+      background-color: #333;
+    }
+    h1 {
+      font-size: 36px;
+      font-weight: bold;
+    }
+    p {
+      font-size: 16px;
+    }
+  </style>
+</head>
+<body>
+  <nav>
+    <a href="/home">Home</a>
+    <a href="/about">About Us</a>
+    <a href="/contact">Contact Us</a>
+    <a href="/products">Products</a>
+    <a href="/services">Services</a>
+    <a href="/profile">Profile</a>
+  </nav>
+  <h1>Welcome to the Homepage</h1>
+  <p>This is the homepage of the website. Here you can find information about the website and its contents.</p>
+  <div id="profile">
+    <h2>Profile</h2>
+    <p>Name: John Doe</p>
+    <p>Email: john.doe@example.com</p>
+    <p>Phone: 123-456-7890</p>
+  </div>
+</body>
+</html>
