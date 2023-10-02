df33a03fc33b74922de0bcd6ca3a1f071a19bbfc
Ahmad Hazim <hazimrizal6@gmail.com>
2023-10-02 08:07:47 +0800

    Page correction

--- index.php
+++ index.php
@@ -188,50 +188,50 @@ if (isset($_SESSION['CustEmail'])) {
         </h2>
       </div>
       <div class="row">
-        <div class=" col-sm-8 col-md-6 px-0">
+        <div class=" col-sm-6 col-md-4 px-0">
           <div class="img-box">
-            <img src="images/G1.jpg" alt="">
-            <a href="images/G1.jpg" data-toggle="lightbox" data-gallery="gallery">
+            <img src="images/g1.jpg" alt="">
+            <a href="images/g1.jpg" data-toggle="lightbox" data-title="Birthday Party" data-gallery="gallery">
               <i class="fa fa-picture-o" aria-hidden="true"></i>
             </a>
           </div>
         </div>
-        <div class="col-sm-8 col-md-6 px-0">
+        <div class="col-sm-6 col-md-4 px-0">
           <div class="img-box">
-            <img src="images/G2.jpg" alt="">
-            <a href="images/G2.jpg" data-toggle="lightbox" data-gallery="gallery">
+            <img src="images/g2.jpg" alt="">
+            <a href="images/g2.jpg" data-toggle="lightbox" data-title="Reunion" data-gallery="gallery">
               <i class="fa fa-picture-o" aria-hidden="true"></i>
             </a>
           </div>
         </div>
-        <div class="col-sm-8 col-md-6 px-0">
+        <div class="col-sm-6 col-md-4 px-0">
           <div class="img-box">
             <img src="images/g3.jpg" alt="">
-            <a href="images/g3.jpg" data-toggle="lightbox" data-gallery="gallery">
+            <a href="images/g3.jpg" data-toggle="lightbox" data-title="Wedding" data-gallery="gallery">
               <i class="fa fa-picture-o" aria-hidden="true"></i>
             </a>
           </div>
         </div>
-        <div class="col-sm-8 col-md-6 px-0">
+        <div class="col-sm-6 col-md-4 px-0">
           <div class="img-box">
             <img src="images/g4.jpg" alt="">
-            <a href="images/g4.jpg" data-toggle="lightbox" data-gallery="gallery">
+            <a href="images/g4.jpg" data-toggle="lightbox" data-title="Wedding" data-gallery="gallery">
               <i class="fa fa-picture-o" aria-hidden="true"></i>
             </a>
           </div>
         </div>
-        <div class="col-sm6 col-md-6 px-0">
+        <div class="col-sm-6 col-md-4 px-0">
           <div class="img-box">
             <img src="images/g5.jpg" alt="">
-            <a href="images/g5.jpg" data-toggle="lightbox" data-gallery="gallery">
+            <a href="images/g5.jpg" data-toggle="lightbox" data-title="Conference" data-gallery="gallery">
               <i class="fa fa-picture-o" aria-hidden="true"></i>
             </a>
           </div>
         </div>
-        <div class="col-sm-8 col-md-6 px-0">
+        <div class="col-sm-6 col-md-4 px-0">
           <div class="img-box">
             <img src="images/g6.jpg" alt="">
-            <a href="images/g6.jpg" data-toggle="lightbox" data-gallery="gallery">
+            <a href="images/g6.jpg" data-toggle="lightbox" data-title="Seminar" data-gallery="gallery">
               <i class="fa fa-picture-o" aria-hidden="true"></i>
             </a>
           </div>
